0ca74e50fcf292e7cb33c413543693d3eb3208fc
Sebastian Mohr <sebastian.mohr@synnotech.de>
2022-02-15 13:18:23 +0100

    Added tests for the 3xx HTTP status codes (redirect)

---
+++ Code/Synnotech.AspNetCore.MinimalApis.Tests/HttpResponseRedirectStatusTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Synnotech.AspNetCore.MinimalApis.Tests;
+
+public class HttpResponseRedirectStatusTests : HttpResponseTestsBase
+{
+    // Status Code 307 Temporary Redirect
+    [Fact]
+    public async Task RedirectTemporaryTest()
+    {
+        var response = await GetHttpResponseMessageFromApi("/redirect/temporary");
+        var responseLocation = getRedirectUriFormHttpResponseMessage(response);
+
+        // statusCodes always is 404 because the redirect url can not be found
+        response.Should().NotBeNull();
+        responseLocation.Should().NotBeNull().And.Be(Location);
+    }
+
+    // Status Code 308 Permanent Redirect
+    [Fact]
+    public async Task RedirectPermanentTest()
+    {
+        var response = await GetHttpResponseMessageFromApi("/redirect/permanent");
+        var responseLocation = getRedirectUriFormHttpResponseMessage(response);
+
+        // statusCodes always is 404 because the redirect url can not be found
+        response.Should().NotBeNull();
+        responseLocation.Should().NotBeNull().And.Be(Location);
+    }
+
+    private Uri? getRedirectUriFormHttpResponseMessage(HttpResponseMessage response)
+    {
+        return response.RequestMessage?.RequestUri;
+    }
+}
