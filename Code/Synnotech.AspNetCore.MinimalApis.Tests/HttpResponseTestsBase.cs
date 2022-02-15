f1549acf1f6f2f63db5a896fd516c2b2251811a1
Sebastian Mohr <sebastian.mohr@synnotech.de>
2022-02-15 13:16:54 +0100

    Added base class for tests, providing test values and environment setup

---
+++ Code/Synnotech.AspNetCore.MinimalApis.Tests/HttpResponseTestsBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Synnotech.AspNetCore.MinimalApis.Tests;
+
+public abstract class HttpResponseTestsBase
+{
+    protected readonly HttpClient Client;
+
+    // test values that only are used for current test environment
+    protected const string Value = "Test";
+    protected static readonly Uri Location = new("http://test.url");
+
+    protected HttpResponseTestsBase()
+    {
+        // start in-Memory testServer for simple integration testing
+        var app = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        Client = app.CreateClient();
+    }
+
+    protected Uri? GetUriFromHttpResponseMessage(HttpResponseMessage response)
+    {
+        return response.Headers.Location;
+    }
+
+    protected async Task<string> GetStringContentFromHttpResponseMessage(HttpResponseMessage response)
+    {
+        var responseString = await response.Content.ReadAsStringAsync();
+
+        // response strings always have quotation marks at the start and end
+        // for cleaner testing they get trimmed in this method
+        responseString = responseString.Trim('\"');
+
+        return responseString;
+    }
+
+    protected async Task<HttpResponseMessage> GetHttpResponseMessageFromApi(string url)
+    {
+        return await Client.GetAsync("/api" + url);
+    }
+}
