63cbf06e82369459c77f9c10388c67981a3b7ac5
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-02-23 10:05:51 +0100

    Updated success status tests to work with new base class

--- Code/Synnotech.AspNetCore.MinimalApis.Tests/HttpResponseSuccessStatusTests.cs
+++ Code/Synnotech.AspNetCore.MinimalApis.Tests/HttpResponseSuccessStatusTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Xunit;
@@ -12,106 +13,103 @@ public class HttpResponseSuccessStatusTests : BaseWebAppTest
 {
     public HttpResponseSuccessStatusTests(ITestOutputHelper output) : base(output) { }
 
-    // // Status Code 200 OK
-    // [Fact]
-    // public async Task OkWithoutBodyTest()
-    // {
-    //     // act
-    //     var response = await GetHttpResponseMessageFromApi("/ok");
-    //     var responseValue = await GetAndFormatStringContentFromHttpResponseMessage(response);
-    //
-    //     // assert
-    //     response.StatusCode.Should().Be(HttpStatusCode.OK);
-    //     responseValue.Should().BeNullOrEmpty();
-    // }
-    //
-    // [Fact]
-    // public async Task OkWithBodyTest()
-    // {
-    //     // act
-    //     var response = await GetHttpResponseMessageFromApi("/ok/body");
-    //     var responseValue = await GetAndFormatStringContentFromHttpResponseMessage(response);
-    //
-    //     // assert
-    //     response.StatusCode.Should().Be(HttpStatusCode.OK);
-    //     responseValue.Should().NotBeNullOrEmpty().And.Be(Value);
-    // }
-    //
-    // // Status Code 201 Created
-    // [Fact]
-    // public async Task CreatedWithStringAsLocationTest()
-    // {
-    //     var response = await GetHttpResponseMessageFromApi("/created/string");
-    //     var responseValue = await GetAndFormatStringContentFromHttpResponseMessage(response);
-    //     var responseUrl = GetUriFromHttpResponseMessage(response);
-    //
-    //     response.StatusCode.Should().Be(HttpStatusCode.Created);
-    //     responseValue.Should().NotBeNullOrEmpty().And.Be(Value);
-    //     responseUrl.Should().NotBeNull().And.Be(Location);
-    // }
-    //
-    // [Fact]
-    // public async Task CreatedWithUriAsLocationTest()
-    // {
-    //     var response = await GetHttpResponseMessageFromApi("/created/uri");
-    //     var responseValue = await GetAndFormatStringContentFromHttpResponseMessage(response);
-    //     var responseUrl = GetUriFromHttpResponseMessage(response);
-    //
-    //     response.StatusCode.Should().Be(HttpStatusCode.Created);
-    //     responseValue.Should().NotBeNullOrEmpty().And.Be(Value);
-    //     responseUrl.Should().NotBeNull().And.Be(Location);
-    // }
-    //
-    // // Status Code 202 Accepted
-    // [Fact]
-    // public async Task AcceptedWithoutLocationProvidedTest()
-    // {
-    //     var response = await GetHttpResponseMessageFromApi("/accepted");
-    //     var responseValue = await GetAndFormatStringContentFromHttpResponseMessage(response);
-    //
-    //     response.StatusCode.Should().Be(HttpStatusCode.Accepted);
-    //     responseValue.Should().NotBeNullOrEmpty();
-    //     responseValue.Should().NotBeNull().And.Be(Value);
-    // }
-    //
-    // [Fact]
-    // public async Task AcceptedWithStringAsLocationTest()
-    // {
-    //     var response = await GetHttpResponseMessageFromApi("/accepted/string");
-    //     var responseValue = await GetAndFormatStringContentFromHttpResponseMessage(response);
-    //     var responseUrl = GetUriFromHttpResponseMessage(response);
-    //
-    //     response.StatusCode.Should().Be(HttpStatusCode.Accepted);
-    //     responseValue.Should().NotBeNullOrEmpty().And.Be(Value);
-    //     responseUrl.Should().NotBeNull().And.Be(Location);
-    // }
-    //
-    // [Fact]
-    // public async Task AcceptedWithUriAsLocationTest()
-    // {
-    //     var response = await GetHttpResponseMessageFromApi("/accepted/uri");
-    //     var responseValue = await GetAndFormatStringContentFromHttpResponseMessage(response);
-    //     var responseUrl = GetUriFromHttpResponseMessage(response);
-    //
-    //     response.StatusCode.Should().Be(HttpStatusCode.Accepted);
-    //     responseValue.Should().NotBeNullOrEmpty().And.Be(Value);
-    //     responseUrl.Should().NotBeNull().And.Be(Location);
-    // }
-    //
-    //
-    // // Status Code 204 No Content
-    // [Fact]
-    // public async Task NoContentTest()
-    // {
-    //     // act
-    //     var response = await GetHttpResponseMessageFromApi("/noContent");
-    //
-    //     // assert
-    //     response.StatusCode.Should().Be(HttpStatusCode.NoContent);
-    // }
-    //
-    // private Uri? GetUriFromHttpResponseMessage(HttpResponseMessage response)
-    // {
-    //     return response.Headers.Location;
-    // }
+    // Status Code 200 OK
+    [Fact]
+    public async Task OkWithoutBodyTest()
+    {
+        var response = await HttpClient.GetAsync("/api/ok");
+    
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        (await response.Content.ReadAsStringAsync()).Should().BeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task OkWithBodyTest()
+    {
+        // act
+        var response = await HttpClient.GetAsync("/api/ok/body");
+        var value = await response.Content.ReadFromJsonAsync<Contact>();
+    
+        // assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        value.Should().BeEquivalentTo(Contact.Default);
+    }
+    
+    
+    // Status Code 201 Created
+    [Fact]
+    public async Task CreatedWithStringAsLocationTest()
+    {
+        var response = await HttpClient.GetAsync("/api/created/string");
+        var value = await response.Content.ReadFromJsonAsync<Contact>();
+        var responseUrl = GetUriFromHttpResponseMessage(response);
+    
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        value.Should().BeEquivalentTo(Contact.Default);
+        responseUrl.Should().NotBeNull().And.Be(Location.Default.Url);
+    }
+    
+    [Fact]
+    public async Task CreatedWithUriAsLocationTest()
+    {
+        var response = await HttpClient.GetAsync("/api/created/uri");
+        var value = await response.Content.ReadFromJsonAsync<Contact>();
+        var responseUrl = GetUriFromHttpResponseMessage(response);
+    
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        value.Should().BeEquivalentTo(Contact.Default);
+        responseUrl.Should().NotBeNull().And.Be(Location.Default.Url);
+    }
+    
+    // Status Code 202 Accepted
+    [Fact]
+    public async Task AcceptedWithoutLocationProvidedTest()
+    {
+        var response = await HttpClient.GetAsync("/api/accepted");
+        var value = await response.Content.ReadFromJsonAsync<Contact>();
+    
+        response.StatusCode.Should().Be(HttpStatusCode.Accepted);
+        value.Should().BeEquivalentTo(Contact.Default);
+    }
+    
+    [Fact]
+    public async Task AcceptedWithStringAsLocationTest()
+    {
+        var response = await HttpClient.GetAsync("/api/accepted/string");
+        var value = await response.Content.ReadFromJsonAsync<Contact>();
+        var responseUrl = GetUriFromHttpResponseMessage(response);
+    
+        response.StatusCode.Should().Be(HttpStatusCode.Accepted);
+        value.Should().BeEquivalentTo(Contact.Default);
+        responseUrl.Should().NotBeNull().And.Be(Location.Default.Url);
+    }
+    
+    [Fact]
+    public async Task AcceptedWithUriAsLocationTest()
+    {
+        var response = await HttpClient.GetAsync("/api/accepted/uri");
+        var value = await response.Content.ReadFromJsonAsync<Contact>();
+        var responseUrl = GetUriFromHttpResponseMessage(response);
+    
+        response.StatusCode.Should().Be(HttpStatusCode.Accepted);
+        value.Should().BeEquivalentTo(Contact.Default);
+        responseUrl.Should().NotBeNull().And.Be(Location.Default.Url);
+    }
+    
+    
+    // Status Code 204 No Content
+    [Fact]
+    public async Task NoContentTest()
+    {
+        // act
+        var response = await HttpClient.GetAsync("/api/noContent");
+    
+        // assert
+        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+    
+    private Uri? GetUriFromHttpResponseMessage(HttpResponseMessage response)
+    {
+        return response.Headers.Location;
+    }
 }
