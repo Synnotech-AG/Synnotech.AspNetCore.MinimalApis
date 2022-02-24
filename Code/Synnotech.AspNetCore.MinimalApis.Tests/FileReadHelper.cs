4ed808273bec9c986e650d652b2d52bf8a346bad
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-02-24 12:39:41 +0100

    updated file stream test to work with new base class, added helper class to determine fileLength

---
+++ Code/Synnotech.AspNetCore.MinimalApis.Tests/FileReadHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+
+namespace Synnotech.AspNetCore.MinimalApis.Tests;
+
+public static class FileReadHelper
+{
+    public static long GetFileLength(string filePath)
+    {
+        return new FileInfo(filePath).Length;
+    }
+
+    public static long GetStreamLength(Stream stream)
+    {
+        var originalPosition = 0L;
+        var totalBytesRead = 0L;
+
+        if (stream.CanSeek)
+        {
+            originalPosition = stream.Position;
+            stream.Position = 0;
+        }
+
+        try
+        {
+            var readBuffer = new byte[4096];
+            int bytesRead;
+
+            while ((bytesRead = stream.Read(readBuffer, 0, 4096)) > 0)
+            {
+                totalBytesRead += bytesRead;
+            }
+        }
+        finally
+        {
+            if (stream.CanSeek)
+            {
+                stream.Position = originalPosition;
+            }
+        }
+
+        return totalBytesRead;
+    }
+}
--- Code/Synnotech.AspNetCore.MinimalApis.Tests/HttpResponseFileTests.cs
+++ Code/Synnotech.AspNetCore.MinimalApis.Tests/HttpResponseFileTests.cs
@@ -1,6 +1,7 @@
-﻿using System.Net;
-using System.Threading.Tasks;
+﻿using System;
 using FluentAssertions;
+using Microsoft.AspNetCore.StaticFiles;
+using Synnotech.AspNetCore.MinimalApis.Tests.DefaultValues;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -9,22 +10,24 @@ namespace Synnotech.AspNetCore.MinimalApis.Tests;
 public class HttpResponseFileTests : BaseWebAppTest
 {
     public HttpResponseFileTests(ITestOutputHelper output) : base(output) { }
-    
+
     // Stream Response
-    // [Fact]
-    // public void StreamResponseTest()
-    // {
-    //     var response = GetHttpResponseMessageFromApi("/api/stream");
-    //
-    //     response.Should().NotBeNull();
-    // }
-    //
-    // // Byte Array Response
-    // [Fact]
-    // public void ByteArrayResponseTest()
-    // {
-    //     var response = GetHttpResponseMessageFromApi("/api/bytes");
-    //
-    //     response.Should().NotBeNull();
-    // }
+    [Fact]
+    public void StreamResponseTest()
+    {
+        using var response = HttpClient.GetStreamAsync("/api/stream");
+        var streamLength = FileReadHelper.GetStreamLength(response.Result);
+
+        response.Should().NotBeNull();
+        streamLength.Should().Be(FileReadHelper.GetFileLength(TestFile.ExcelDefault.FilePath!));
+    }
+
+    // Byte Array Response
+    [Fact]
+    public void ByteArrayResponseTest()
+    {
+        using var response = HttpClient.GetAsync("/api/bytes");
+
+        response.Should().NotBeNull();
+    }
 }
